dd661e8936f33c44506eb6948b258818192042ca
Simeon <sgarner96@gmail.com>
2026-04-04 23:40:52 -0600

    Add - tkinter, apple icon

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 ﻿import sqlite3
+import tkinter as tk
 
+# TODO: Create Database
+# TODO: Create GUI
+# TODO: Create layout
 
 def main():
-    pass
+    app = tk.Tk()
+    app.title("Food Calendar")
+    app.geometry("350x200")
+    app.iconphoto(False, tk.PhotoImage(file="apple.ico"))
+
+    app.mainloop()
 
 if __name__ == "__main__":
     main()
-    
+     
