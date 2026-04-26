516e7279873709e6d1b954f5a88c89ebb09c7677
Simeon <gar17052@byui.edu>
2026-04-26 00:50:04 -0600

    Create immutable constants

---
+++ constants.py
@@ -0,0 +1,40 @@
+﻿from dataclasses import dataclass
+from tkinter import RAISED, FLAT
+
+@dataclass(frozen=True)
+class AppConfig:
+    title = "Food Calendar"
+    ico = "apple.ico"
+
+DATABASE = "./food.db"
+
+@dataclass(frozen=True)
+class Button:
+    click = "<Button-1>"
+    release = "<ButtonRelease-1>"
+    color = "white"
+    colorPressed = "green"
+    width = 150
+    height = width
+    borderWidth = 1
+    relief = RAISED
+    reliefPressed = FLAT
+    pad = 1
+
+@dataclass(frozen=True)
+class Table:
+    id = "id"
+
+@dataclass(frozen=True)
+class MealsTable(Table):
+    name = "meals"
+
+@dataclass(frozen=True)
+class DBTypes:
+    integer = "INTEGER"
+    text = "text"
+
+@dataclass(frozen=True)
+class DBAttr:
+    primaryKey = "PRIMARY KEY"
+    notNull = "NOT NULL"
